7c94a0cfb29da4ed9f980ae2a178e84e8683ffab
Gabriel Perinotto <perinotto@crmsolucoes.com.br>
2023-08-18 10:35:42 -0300

    3 - Implementando a possibilide de salvar a foto

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let canDraw = false;
 let currentColor = 'black'
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
+let mouseX = 0;
+let mouseY = 0;
 
 //Events
 
@@ -14,6 +16,7 @@ document.querySelectorAll('.color').forEach((item) => {
 screen.addEventListener("mousedown", mouseDownEvent);
 screen.addEventListener("mousemove", mouseMoveEvent);
 screen.addEventListener("mouseup", mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen)
 
 //Functions
 
@@ -26,23 +29,49 @@ function colorClickEvent(e){
     e.target.classList.add('active');
 }
 
-function mouseDownEvent(){
+function mouseDownEvent(e){
 
     canDraw = true;
 
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
 }
 
 function mouseMoveEvent(e){
 
     if(canDraw){
          
-        //canDraw();
+        draw(e.pageX, e.pageY);
 
     }
 }
 
 function mouseUpEvent(){
 
+
     canDraw = false;
 
+}
+
+function draw(x, y){
+
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen(){
+    ctx.setTransform(1,0,0,1,0,0)
+    ctx.clearRect(0,0,ctx.canvas.width, ctx.canvas.height)
 }
